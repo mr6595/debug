2d97f91d0da2df204d0c82b71802f7d453a6feb0
boomloh <57511315+mr6595@users.noreply.github.com>
2023-03-11 12:28:32 +0800

    Update game-logic.js

--- game-logic.js
+++ game-logic.js
@@ -1,36 +1,105 @@
-// get the text to type
-const quote = document.querySelector('.quote').textContent.trim();
+// Define the quotes to type
+const quotes = [
+  "The quick brown fox jumps over the lazy dog",
+  "The greatest glory in living lies not in never falling, but in rising every time we fall",
+  "The way to get started is to quit talking and begin doing",
+  "If life were predictable it would cease to be life, and be without flavor",
+  "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough"
+];
 
-// start the game
+// Get DOM elements
+const quoteElement = document.querySelector('.quote');
+const inputBox = document.querySelector('.input-box');
+const timerElement = document.querySelector('.timer');
+const wpmElement = document.querySelector('.wpm');
+const accuracyElement = document.querySelector('.accuracy');
+const startButton = document.querySelector('.start-button');
+const resetButton = document.querySelector('.reset-button');
+
+// Define game variables
+let currentQuote = "";
+let quoteIndex = 0;
+let wordCount = 0;
+let errorCount = 0;
 let timer = 0;
 let intervalId;
 let isPlaying = false;
-let wordCount = 0;
-let errorCount = 0;
-const inputBox = document.querySelector('.input-box');
-const timerDisplay = document.querySelector('.timer');
-const wpmDisplay = document.querySelector('.wpm');
-const accuracyDisplay = document.querySelector('.accuracy');
-inputBox.addEventListener('keydown', (e) => {
-  // start the timer if it's not running
-  if (!isPlaying) {
-    isPlaying = true;
-    intervalId = setInterval(() => {
-      timer++;
-      timerDisplay.textContent = timer;
-    }, 1000);
-  }
 
-  // calculate WPM and accuracy
+// Function to start the game
+function startGame() {
+  // Set the current quote and display it
+  currentQuote = quotes[quoteIndex];
+  quoteElement.textContent = currentQuote;
+
+  // Set up the game variables
+  timer = 0;
+  wordCount = 0;
+  errorCount = 0;
+  isPlaying = true;
+
+  // Highlight the first word
+  const words = currentQuote.split(" ");
+  const firstWord = words[0];
+  quoteElement.innerHTML = currentQuote.replace(firstWord, `<span class="highlight">${firstWord}</span> `);
+
+  // Start the timer
+  intervalId = setInterval(() => {
+    timer++;
+    timerElement.textContent = timer;
+  }, 1000);
+
+  // Hide the start button and show the input box
+  startButton.style.display = "none";
+  inputBox.style.display = "block";
+
+  // Set the focus on the input box
+  inputBox.focus();
+}
+
+// Function to reset the game
+function resetGame() {
+  // Stop the timer
+  clearInterval(intervalId);
+
+  // Reset the game variables
+  currentQuote = "";
+  quoteIndex = 0;
+  wordCount = 0;
+  errorCount = 0;
+  timer = 0;
+  isPlaying = false;
+
+  // Reset the DOM elements
+  quoteElement.innerHTML = "";
+  inputBox.value = "";
+  timerElement.textContent = "0";
+  wpmElement.textContent = "0";
+  accuracyElement.textContent = "0";
+  startButton.style.display = "block";
+  inputBox.style.display = "none";
+}
+
+// Function to handle the input box
+function handleInput() {
+  // Get the typed text and split it into words
   const inputText = inputBox.value.trim();
-  if (inputText.endsWith(' ')) {
-    wordCount++;
-    inputBox.value = '';
-    const typedWords = inputText.split(' ');
-    const typedChars = typedWords.reduce((acc, curr) => acc + curr.length, 0);
-    const accuracy = ((typedChars - errorCount) / typedChars) * 100;
-    accuracyDisplay.textContent = accuracy.toFixed(1);
-    const wpm = (wordCount / timer) * 60;
-    wpmDisplay.textContent = wpm.toFixed(1);
-  }
-});
+  const typedWords = inputText.split(" ");
+
+  // Get the current word and the next word
+  const currentWord = typedWords[typedWords.length - 1];
+  const expectedWords = currentQuote.split(" ");
+  const expectedWord = expectedWords[typedWords.length - 1];
+
+  // Check for errors
+  if (currentWord !== expectedWord) {
+    // Add an error class to the input box
+    inputBox.classList.add("error");
+
+    // Increment the error count
+    errorCount++;
+  } else {
+    // Remove the error class from the input box
+    inputBox.classList.remove("error");
+
+    // Highlight the next word
+    const nextWord
